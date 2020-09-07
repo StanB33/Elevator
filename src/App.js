50d41eb16c8a4b1dcef354afedb4d4e493afb867
Stan <stanbekker17@gmail.com>
2020-09-07 20:06:32 +0200

    functional component gemaakt, awaits en string literals verwijdert. de direction werkt niet meer

--- src/App.js
+++ src/App.js
@@ -1,140 +1,156 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Cabin from './Components/Cabin.js'
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-     this.state = {
-       floors: [4,3,2,1,0],
-       activeFloor: 0,
-       targetFloors: [],
-       doors: 'closed',
-       direction: 'none',
-     }
-   }
+function App() {
+  const [floors] = useState([4, 3, 2, 1, 0]);
+  const [activeFloor, setActiveFloor] = useState(0);
+  const [targetFloors, setTargetFloors] = useState([]);
+  const [doors, setDoors] = useState("closed");
+  const [direction, setDirection] = useState("none");
 
-   sleep = (ms) => {
-     return new Promise(
-       resolve => setTimeout(resolve, ms)
-     );
-   }
+  const sleep = (ms) => {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  };
 
-   closeDoor = async () => {
-     if (this.state.targetFloors[this.state.targetFloors.length - 1] > this.state.activeFloor && this.state.direction === 'none') {
-       this.setState({direction: 'up'})
-     } else if (this.state.targetFloors[0] < this.state.activeFloor && this.state.direction === 'none') {
-       this.setState({direction: 'down'})
-     }
-     if (this.state.doors === 'open') {
-        await this.sleep(2000)
-        this.setState({doors: 'closed'})
-      }
-      return null
+  const moveFloor = (newTarget) => {
+    const target = targetFloors;
+    target.push(newTarget);
+    setTargetFloors(target.sort((a, b) => a - b));
+    console.log("a");
+    if (direction === "none" && targetFloors.length === 1) {
+      console.log("aa");
+      theDoors();
     }
+  };
 
-    openDoor = async () => {
-      if (this.state.doors === 'closed' && this.state.targetFloors.includes(`${this.state.activeFloor}`)) {
-        await this.sleep(2000)
-        let traveled = await this.state.targetFloors.filter(item => item !== `${this.state.activeFloor}`);
-        this.setState({doors: 'open', targetFloors: traveled})
-     }
-     return null
-   }
+  const theDoors = async () => {
+    console.log("c");
+    if (doors === "closed" && targetFloors.includes(`${activeFloor}`)) {
+      setDoors("open");
+      await sleep(2000);
+      setDoors("closed");
+      let traveled = targetFloors.filter((item) => item != activeFloor);
+      setTargetFloors(traveled);
+      await sleep(2000);
+    }
+    traveling();
+  };
 
-     up = async () => {
-       await this.sleep(2000)
-       let up = this.state.activeFloor
-       up++
-       this.setState({activeFloor: up})
-       if (this.state.targetFloors[this.state.targetFloors.length - 1] == this.state.activeFloor){
-         this.setState({direction: 'none'})
-       }
-       return null
-     }
+  const traveling = async () => {
+    console.log("b");
+    await directionToGo();
+    console.log("e");
+    await sleep(2000);
+    console.log(direction);
+    if (direction === "up" && doors === "closed") {
+      setActiveFloor(activeFloor + 1);
+      traveling();
+    }
+    if (direction === "down" && doors === "closed") {
+      setActiveFloor(activeFloor - 1);
+      console.log("f");
+    }
+    console.log("g");
+    console.log(floors);
+    console.log(activeFloor);
+    console.log(targetFloors);
+    console.log(doors);
+    console.log(direction);
+  };
 
-      down = async () => {
-       await this.sleep(2000)
-       let down = this.state.activeFloor
-       down--
-       this.setState({activeFloor: down})
-       if (this.state.targetFloors[0] == this.state.activeFloor){
-         this.setState({direction: 'none'})
-       }return null
-      }
+  const directionToGo = async () => {
+    console.log("d");
+    if (
+      targetFloors[targetFloors.length - 1] > activeFloor &&
+      direction === "none"
+    ) {
+      setDirection("up");
+      console.log("ddd");
+    } else if (targetFloors[0] < activeFloor && direction === "none") {
+      const test = "down";
+      setDirection(test);
+      console.log("dd");
+    }
+  };
 
-      moveFloor = async (newTarget)  => {
-        const target = this.state.targetFloors
-        target.push(newTarget)
-        target.sort((a, b) => a - b);
-        this.setState({targetFloors: target})
-        if (newTarget == this.state.activeFloor && this.state.direction === 'none'){
-          this.openDoor()
-        }
-        if (this.state.direction === 'none' && this.state.targetFloors.length == 1){
-            await this.traveling()
-        }
-      }
-
-    traveling = async () => {
-      await this.closeDoor()
-     if (this.state.direction === 'up' && this.state.doors === 'closed'){
-         await this.up()
-         await this.openDoor()
-         await this.closeDoor()
-         await this.traveling()
-
-     }
-     if (this.state.direction === 'down' &&  this.state.doors === 'closed'){
-         await this.down()
-         await this.openDoor()
-         await this.closeDoor()
-         await this.traveling()
-
-     }
-
-   }
-
-render() {
-
-  const floorButtons = this.state.floors.map((floor) => {
-    if (this.state.activeFloor === floor && this.state.doors === 'open'){
-      return <div className='container'>
-      <div className='buttonContainer'>
-      <button className="floorButton" value={floor} onClick={(e) => this.moveFloor(e.target.value)}> O </button>
-      </div>
-      <div className="cabinOpen">
-          <div className='cabinFloor'>{floor}</div>
-        <Cabin floors={this.state.floors} activeFloor={this.state.activeFloor} targetFloors={this.state.targetFloors} moveFloor={this.moveFloor}/>
+  const floorButtons = floors.map((floor) => {
+    if (activeFloor === floor && doors === "open") {
+      return (
+        <div className="container" key={floor}>
+          <div className="buttonContainer">
+            <button
+              className="floorButton"
+              value={floor}
+              onClick={(e) => moveFloor(e.target.value)}
+            >
+              {" "}
+              O{" "}
+            </button>
+          </div>
+          <div className="cabinOpen">
+            <div className="cabinFloor">{floor}</div>
+            <Cabin
+              floors={floors}
+              activeFloor={activeFloor}
+              targetFloors={targetFloors}
+              moveFloor={moveFloor}
+            />
+          </div>
         </div>
-      </div>
-    } else if (this.state.activeFloor === floor) {
-      return <div className='container'>
-      <div className='buttonContainer'>
-        <button className="floorButton" value={floor} onClick={(e) => this.moveFloor(e.target.value)}> O </button>
-      </div>
-      <div className="cabinClosed">
-        <div className='cabinFloor'>{floor}</div>
-        <Cabin floors={this.state.floors} activeFloor={this.state.activeFloor} targetFloors={this.state.targetFloors} moveFloor={this.moveFloor}/>
+      );
+    } else if (activeFloor === floor) {
+      return (
+        <div className="container" key={floor}>
+          <div className="buttonContainer">
+            <button
+              className="floorButton"
+              value={floor}
+              onClick={(e) => moveFloor(e.target.value)}
+            >
+              {" "}
+              O{" "}
+            </button>
+          </div>
+          <div className="cabinClosed">
+            <div className="cabinFloor">{floor}</div>
+            <Cabin
+              floors={floors}
+              activeFloor={activeFloor}
+              targetFloors={targetFloors}
+              moveFloor={moveFloor}
+            />
+          </div>
         </div>
-      </div>
+      );
     } else {
-      return <div className="container">
-        <div className='buttonContainer'>
-        <button className="floorButton" value={floor} onClick={(e) => this.moveFloor(e.target.value)}> O </button>
+      return (
+        <div className="container" key={floor}>
+          <div className="buttonContainer">
+            <button
+              className="floorButton"
+              value={floor}
+              onClick={(e) => moveFloor(e.target.value)}
+            >
+              {" "}
+              O{" "}
+            </button>
+          </div>
+          <div className="cabin">
+            <div className="cabinFloor">{floor}</div>
+          </div>
         </div>
-        <div className="cabin">
-          <div className='cabinFloor'>{floor}</div>
-        </div>
-      </div>
+      );
     }
-  })
+  });
 
-  return <div>
+  return (
+    <div>
+      {targetFloors[0]} <br />
+      {direction} <br />
+      {activeFloor}
       {floorButtons}
     </div>
-  }
+  );
 }
-
-
 
 export default App;
--- src/Components/Cabin.js
+++ src/Components/Cabin.js
@@ -2,17 +2,13 @@ import React from 'react';
 
 
 function Cabin(props){
-  const [floors, setFloors] = React.useState(props.floors)
-  const [active, setActive] = React.useState(props.activeFloor)
-  const [target, setTarget] = React.useState(props.targetFloor)
-
 
   const moveFloor = (newTarget) => {
     props.moveFloor(newTarget)
   }
 
-  const floorButtons = floors.map((floor) => {
-      return <button value={floor} onClick={(e) => moveFloor(e.target.value)}> {floor} </button>
+  const floorButtons = props.floors.map((floor) => {
+      return <button value={floor} key={floor} onClick={(e) => moveFloor(e.target.value)}> {floor} </button>
   })
 
 
